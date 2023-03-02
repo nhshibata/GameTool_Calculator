f88717a80f20f717a0fc0e9b20a891478d298c92
nhshibata <dragon23633@gmail.com>
2023-03-02 18:21:44 +0900

    json形式再利用 RateDataのみではあるものの、UI上で変更、追加、選択機能実装

---
+++ GameTool_Calculator/enemyData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.Serialization;
+
+namespace GameTool_Calculator
+{
+
+    [DataContract] // シリアライズ用属性
+    class CEnemyData : jsonData
+    {
+        [DataMember]
+        string m_strName;
+
+        [DataMember]
+        int m_nAtk = 2;
+
+        [DataMember]
+        int m_nHp = 2;
+
+        [DataMember]
+        int m_nMoney = 2;
+
+        public string Name { get => m_strName; set => m_strName = value; }
+        public int Atk { get => m_nAtk; set => m_nAtk = value; }
+        public int Hp { get => m_nHp; set => m_nHp = value; }
+        public int Money { get => m_nMoney; set => m_nMoney = value; }
+
+        protected override void ReadCallBack(object json)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ GameTool_Calculator/itemData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.Serialization;
+
+namespace GameTool_Calculator
+{
+    [DataContract] // シリアライズ用属性
+
+    class CItemData : jsonData
+    {
+        [DataMember]
+        string m_strName;
+
+        [DataMember]
+        int m_nMoney = 2;
+
+        public string Name { get => m_strName; set => m_strName = value; }
+        public int Money { get => m_nMoney; set => m_nMoney = value; }
+
+
+        protected override void ReadCallBack(object json)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
